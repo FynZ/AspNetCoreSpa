e12fab427d48bae7f82a5831b2f7f956387e4f54
vdebote <vdebote@veepee.com>
2021-09-18 23:12:36 +0200

    feat(Auth): More shit

--- src/AspNetCoreSpa.Front/pages/Callback.vue
+++ src/AspNetCoreSpa.Front/pages/Callback.vue
@@ -1,4 +1,10 @@
 <template>
+  <main>
     <p>callback page</p>
+
+    <NuxtLink to="/">
+          Return  to home page
+    </NuxtLink>
+  </main>
 </template>
 
--- src/AspNetCoreSpa.Front/pages/index.vue
+++ src/AspNetCoreSpa.Front/pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <main>
-    <figure class="bg-gray-100 rounded-xl p-8">
-      <img class="w-32 h-32 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
+    <div v-if='$auth.loggedIn'>
+      <figure class="bg-gray-100 rounded-xl p-8">
         <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
           <NuxtLink to="/login">
             Login
           </NuxtLink>
-      </div>
-    </figure>
+        </div>
+      </figure>
+    </div>
+    <div v-else>You are {{ $auth.user }}</div>
     <Tutorial/>
   </main>
 </template>
